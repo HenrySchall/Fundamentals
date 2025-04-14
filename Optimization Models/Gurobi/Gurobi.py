68bf4647ab9761491b47c00e904ccec241d7a1de
HenrySchall <henrique.schall@outlook.com>
2025-04-14 18:44:04 -0300

    .

--- Optimization Models/Gurobi/Gurobi.py
+++ Optimization Models/Gurobi/Gurobi.py
@@ -22,7 +22,19 @@ y3 = m.addConstr(1.2*x1 + 2.0*x2 + 2.4*x3 <= 250)
 
 m.optimize
 
-# Combinação ideal de cada variável
+# Combinação ideal de produtos
+print("Produto A:", x1.X)
+print("Produto B:", x2.X)
+print("Produto C:", x3.X)
+
+# Tempo de uso da máquina ideal de produtos
+tempo_m1 = 120 - c1.Slack
+tempo_m2 = 200 - c2.Slack
+tempo_m3 = 250 - c3.Slack
+
+print("Máquina_1", tempo_m1)
+print("Máquina_2", tempo_m2)
+print("Máquina_3", tempo_m3)
 
 #################
 ### Example 2 ###
