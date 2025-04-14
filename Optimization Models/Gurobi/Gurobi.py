f7935c2206fe21af17424f4a13465eaf2816be96
HenrySchall <henrique.schall@outlook.com>
2025-04-14 18:58:52 -0300

    .

--- Optimization Models/Gurobi/Gurobi.py
+++ Optimization Models/Gurobi/Gurobi.py
@@ -4,16 +4,14 @@ import gurobipy as gp
 ### Example 1 ###
 #################
 
-Model = gp.Model()
-
-x1 = m.addVar()
-x2 = m.addVar()
-x3 = m.addVar()
+x1 = gp.addVar()
+x2 = gp.addVar()
+x3 = gp.addVar()
 
 m.setObjective
 
 # Função Objeto
-m.setObjective(12*x1 + 18*x2 + 22*x3, sense=gp.GRB.Maximize)
+gp.setObjective(12*x1 + 18*x2 + 22*x3, sense = gp.GRB.Maximize)
 
 # Restrições
 y1 = m.addConstr(1.5*x1 + 1.2*x3 <= 120)
@@ -39,3 +37,32 @@ print("Máquina_3", tempo_m3)
 #################
 ### Example 2 ###
 #################
+
+itens = 15
+capacidade = 1000
+
+# Rotulagem
+itens = list()
+for i in range (itens):
+    rotulo = "Item_{}".format(i+1)
+    itens.append(rotulo)
+
+vetor_pesos = [65,94,119,59,149,114,57,136,100,150,122,117,120,130,133]
+valores = dict()
+for index, valor in enumerate(vetor_pesos):
+    rotulo = itens[index]
+    valores[rotulo] = valor
+
+vetor_pesos = [455,691,833,425,1064,758,419,914,651,966,828,827,857,837,894]
+valores = dict()
+for index, valor in enumerate(vetor_pesos):
+    rotulo = itens[index]
+    valores[rotulo] = valor
+
+print(valores)
+
+# Função Objeto
+x = gp.addVars(itens, vtype=gp.GBR.BINARY)
+gp.setObjective(gp.quicksum(x[i] * valores[i] for i in itens), sense = gp.GRB.Maximize)
+
+# Restrição
