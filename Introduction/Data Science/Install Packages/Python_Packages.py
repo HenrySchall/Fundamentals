08cceeb87e1a135ecfca900112f889768f1eb50b
HenrySchall <henrique.schall@outlook.com>
2025-04-20 02:37:43 -0300

    .

--- Introduction/Data Science/Install Packages/Python_Packages.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-# Lista de pacotes que você quer instalar
-pacotes = ["numpy", "pandas", 'matplotlib', "scipy.stats","seaborn","statsmodels","plotly"] 
-
-for pacote in pacotes:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", pacote])
